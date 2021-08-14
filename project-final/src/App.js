5b3e0ddbd0438f609162196c54de5b59a7bf02a5
glyn <glynlewington@gmail.com>
2021-08-14 11:43:56 +1000

    updated exit animation logic && added uuid

--- project-final/src/App.js
+++ project-final/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from "react";
-import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import React, { useState, useRef, useEffect } from "react";
+import { motion, AnimatePresence, useIsPresent } from "framer-motion";
+import { v4 as uuid } from "uuid";
 
 const App = () => {
   const [cards, setCards] = useState(defaultCards);
@@ -7,7 +8,7 @@ const App = () => {
   async function addCard() {
     const res = await fetch(`https://source.unsplash.com/random/300x400`);
     if (!res.ok) return;
-    const id = cards.length === 0 ? 0 : cards[cards.length - 1].id + 1;
+    const id = uuid();
     const card = { id, img: res.url };
     setCards([...cards, card]);
   }
@@ -83,28 +84,29 @@ const variants = {
 };
 
 const Card = ({ card, removeCard, moveCard, i }) => {
-  const [isRemoving, setIsRemoving] = useState(false);
   const [position, setPosition] = useState({ left: 0, top: 0 });
   const cardRef = useRef(null);
 
   function handleRemove() {
     const position = cardRef.current.getBoundingClientRect();
     setPosition(position);
-    setIsRemoving(true);
     removeCard(card.id);
   }
 
+  const isPresent = useIsPresent();
+
   return (
     <motion.div
+      layout
       initial={{ x: "-300px", opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       exit={{ y: "-300px", opacity: 0 }}
       className="card"
       style={{
         width: "18rem",
-        position: isRemoving ? "absolute" : "block",
-        left: isRemoving ? position.left : "auto",
-        top: isRemoving ? position.top : "auto",
+        position: !isPresent ? "absolute" : "block",
+        left: !isPresent ? position.left : "auto",
+        top: !isPresent ? position.top : "auto",
         padding: "20px",
       }}
       ref={cardRef}
@@ -143,11 +145,15 @@ const Card = ({ card, removeCard, moveCard, i }) => {
 
 const defaultCards = [
   {
-    id: 0,
+    id: uuid(),
     img: "https://images.unsplash.com/photo-1587900437942-8758241767ef?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=300",
   },
   {
-    id: 1,
+    id: uuid(),
     img: "https://images.unsplash.com/photo-1586336900429-71f0642f66fd?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=300",
+  },
+  {
+    id: uuid(),
+    img: "https://images.unsplash.com/photo-1587900437942-8758241767ef?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=300",
   },
 ];
